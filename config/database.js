0de8e10e1b382f9aec807c3c6ef50095a0d41b53
cubocode <cubo.code.dev@gmail.com>
2026-07-13 21:33:46 -0300

    segundo commit

--- config/database.js
+++ config/database.js
@@ -1,9 +1,17 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "../.env") });
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-});
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    String(process.env.DB_PASSWORD || ""),
+    {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 5432,
+        dialect: "postgres",
+        logging: false,
+    }
+);
 
 module.exports = sequelize;
